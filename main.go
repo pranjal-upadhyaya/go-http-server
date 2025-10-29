2893c7183ff199cafd7041683ec18a45ed2089f4
pranjal <rktpranjal@gmail.com>
2025-10-30 01:47:35 +0530

    created helper funcs to respond with error and json, implemented msg cleaning func

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"slices"
+	"strings"
 	"sync/atomic"
 )
 
@@ -22,6 +25,8 @@ func main() {
 	mux.Handle("GET /admin/metrics", apiCfg.loadServerHits())
 
 	mux.Handle("POST /admin/reset", apiCfg.resetServerHits())
+
+	mux.Handle("POST /api/validate_chirp", validateChirp())
 
 	server := http.Server{}
 
@@ -100,4 +105,99 @@ func (hc HealthCheck) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	body := []byte(hc.body)
 
 	rw.Write(body)
+}
+
+type requestFormat struct {
+	Body string `json:"body"`
+}
+
+type successfulResponseFormat struct {
+	CleanedBody string `json:"cleaned_body"`
+}
+
+type errorResponseFormat struct {
+	Error string `json:"error"`
+}
+
+func validateChirp() http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		parsedRequest := requestFormat{}
+
+		requestBody := r.Body
+		decoder := json.NewDecoder(requestBody)
+		err := decoder.Decode(&parsedRequest)
+
+		if err != nil {
+
+			errMsg := fmt.Sprintf("Error decoding request: %s", err)
+
+			respondWithError(w, 500, errMsg)
+
+			return
+		}
+
+		if len(parsedRequest.Body) > 140 {
+
+			errMsg := "Chirp is too long"
+
+			respondWithError(w, 400, errMsg)
+
+			return
+
+		}
+
+		parsedResponse := successfulResponseFormat{
+			CleanedBody: cleanMsg(parsedRequest.Body),
+		}
+
+		respondWithJson(w, 200, parsedResponse)
+	})
+}
+
+func respondWithError(w http.ResponseWriter, statusCode int, msg string) {
+	parsedError := errorResponseFormat{
+		Error: msg,
+	}
+
+	res, _ := json.Marshal(parsedError)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(res)
+}
+
+func respondWithJson(w http.ResponseWriter, statusCode int, payload interface{}) {
+	res, err := json.Marshal(&payload)
+	if err != nil {
+
+		errMsg := fmt.Sprintf("Error marshaling response: %s", err)
+
+		respondWithError(w, 500, errMsg)
+
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(res)
+}
+
+func cleanMsg(msg string) string {
+
+	replacementWord := "****"
+
+	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
+
+	msgSlice := strings.Split(msg, " ")
+
+	for index, word := range msgSlice {
+		if lowerString := strings.ToLower(word); slices.Contains(profaneWords, lowerString) {
+			msgSlice[index] = replacementWord
+		}
+	}
+
+	cleanedMsg := strings.Join(msgSlice, " ")
+
+	return cleanedMsg
+
 }
